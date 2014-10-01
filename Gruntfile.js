4b2bc18f44a1f30b7e3b10ed3f3fd66249eaa7f9
Kyle Bock <kyle.bock@oracle.com>
2014-10-01 13:19:32 -0400

    adding grunt for running jshint as well as adding the pre-commit jshint task

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,21 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    // Clean stuff up
+    clean: {
+      // Clean any pre-commit hooks in .git/hooks directory
+      hooks: ['.git/hooks/pre-commit']
+    },
+
+    // Run shell commands
+    shell: {
+      hooks: {
+        // Copy the project's pre-commit hook into .git/hooks
+        command: 'cp config/git-hooks/pre-commit .git/hooks/'
+      }
+    },
+
     jshint: {
       all: ['app/**/*.js'],
       options: {
@@ -42,8 +57,11 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-shell');
 
   // Default task(s).
   grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('hookmeup', ['clean:hooks', 'shell:hooks']);
 
 };
